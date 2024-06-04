22db4353287123c4e425a007cc1d974e4f207c8b
Darezma <mahendradaffareza@gmail.com>
2024-06-04 21:17:07 +0700

    useContext

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -6,12 +6,7 @@ const Todos = ({ todos, toggleCompleted, deleteTodo }) => {
     <div style={styles.container}>
       {todos.map((todo) => {
         return (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            toggleCompleted={toggleCompleted}
-            deleteTodo={deleteTodo}
-          />
+          <TodoItem key={todo.id} todo={todo} />
         )
       })}
     </div>
